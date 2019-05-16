55f454810deb2f6199a106fa33103372a960106a
thomscot <tommaso.scotti@quoine.com>
2019-05-16 16:57:28 +0900

    Now pylint 10.00/10

--- bfxapi/tests/test_rest_movements.py
+++ bfxapi/tests/test_rest_movements.py
@@ -1,22 +1,36 @@
-from bfxapi.rest.BfxRest import BfxRest
-import unittest
-import asyncio
+"""
+Module to test rest methods
+"""
+
 import os
-from dateutil.tz import tzutc
+import asyncio
+import unittest
 from datetime import datetime
+from dateutil.tz import tzutc
+from bfxapi.rest.BfxRest import BfxRest
 
 
 class TestBfxRest(unittest.TestCase):
+    """
+    Tests for BfxRest module
+    """
+
     API_KEY = os.getenv('BALANCE_TRACER_BITFINEX_KEY')
     API_SECRET = os.getenv('BALANCE_TRACER_BITFINEX_SECRET')
 
 
     def test_get_currency_movements(self):
+        """
+        Test get_currency movements
+        """
         loop = asyncio.new_event_loop()
         bfx = BfxRest(self.API_KEY, self.API_SECRET, loop=loop, logLevel='DEBUG')
 
-        movements = asyncio.run(bfx.get_currency_movements('BTC', datetime(2015, 9, 1, tzinfo=tzutc()),
-                                                           datetime(2019, 5, 1, tzinfo=tzutc())))
+        movements = asyncio.run(bfx.get_currency_movements('BTC',
+                                                           datetime(2015, 9, 1, tzinfo=tzutc()),
+                                                           datetime(2019, 5, 1, tzinfo=tzutc())
+                                                           )
+                                )
         self.assertIsNotNone(movements)
 
         loop.close()
